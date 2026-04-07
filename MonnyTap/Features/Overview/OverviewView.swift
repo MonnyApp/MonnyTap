87564f1ada074c8b78ea6430c8c8d32ab0c58714
fikrahdamars <fikrahdamar7@gmail.com>
2026-04-07 16:59:52 +0700

    cleaning repository

---
+++ MonnyTap/Features/Overview/OverviewView.swift
@@ -0,0 +1,18 @@
+//
+//  OverviewView.swift
+//  MonnyTap
+//
+//  Created by Fikrah Damar Huda on 07/04/26.
+//
+
+import SwiftUI
+
+struct OverviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OverviewView()
+}
---
+++ MonnyTap/Features/TransactionsDetail/TransactionsDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  TransactionsDetailView.swift
+//  MonnyTap
+//
+//  Created by Fikrah Damar Huda on 07/04/26.
+//
+
+import SwiftUI
+
+struct TransactionsDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransactionsDetailView()
+}
