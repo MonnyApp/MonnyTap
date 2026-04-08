3b22853457fdb314c8b13c507162e0ef5bd56db0
Shannon Devona <shannon.dev22@gmail.com>
2026-04-08 17:28:38 +0700

    component: expense card transaction

--- MonnyTap/Components/TransactionCard.swift
+++ MonnyTap/Components/TransactionCard.swift
@@ -9,7 +9,39 @@ import SwiftUI
 
 struct TransactionCard: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 16) {
+
+            VStack(spacing: 4) {
+                ZStack {
+                    Circle()
+                        .fill(Color.yellowmonny)
+                        .frame(width: 52, height: 52)
+                    Image(systemName: "fork.knife")
+                        .foregroundColor(.white)
+                        .font(.system(size: 22))
+                }
+                Text("FnB")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+            }
+
+            Spacer()
+
+            VStack(alignment: .trailing, spacing: 4) {
+                Text("Ayam Geprek Wani")
+                    .font(.subheadline)
+                Text("Rp 13.000")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.red)
+            }
+        }
+        .padding(.horizontal, 30)
+        .padding(.vertical, 16)
+        .background(Color(.systemBackground))
+        .cornerRadius(50)
+        .shadow(color: .black.opacity(0.06), radius: 8, x: 0, y: 2)
+        .padding(.horizontal)
     }
 }
 
