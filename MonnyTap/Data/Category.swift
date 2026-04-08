c9196783867552b23bf2d9362a04ddb9eb3d971c
ibnuck <masitoh7564@gmail.com>
2026-04-08 09:41:00 +0700

    Initialize: expense category feature branch

---
+++ MonnyTap/Data/Category.swift
@@ -0,0 +1,8 @@
+//
+//  Category.swift
+//  MonnyTap
+//
+//  Created by Ibnu Taufick Ahraza on 07/04/26.
+//
+
+import Foundation
---
+++ MonnyTap/Features/Category/ModalCategoryExpenseView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryExpense.swift
+//  MonnyTap
+//
+//  Created by Ibnu Taufick Ahraza on 07/04/26.
+//
+
+import SwiftUI
+
+struct CategoryExpense: View {
+    var body: some View {
+        Text("CategoryExpense") 
+    }
+}
+
+#Preview {
+    CategoryExpense()
+}
